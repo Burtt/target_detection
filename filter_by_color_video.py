672da20909faabe5525266526d078c78adc4aa82
Tyler Burt <tyler.burtt@gmail.com>
2017-10-22 13:11:48 -0400

    change to reading camera instead of static image

---
+++ filter_by_color_video.py
@@ -0,0 +1,82 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    ret, frame = cap.read()
+    sourceImage = frame
+    #sourceImage = cv2.imread("RBY_Targets.jpg")
+    #convert pixel ratio to resize image
+    #ratio = sourceImage.shape[0] / 300.0
+    #orig = sourceImage.copy()
+    #sourceImage = imutils.resize(sourceImage, height = 300)
+    #edgesOnlyImage = cv2.Canny(sourceImage, 150,250)
+
+    #show image with edges only to test if it is working
+    #cv2.imshow("edgish", edgesOnlyImage)
+    #cv2.waitKey(0)
+
+    #create an array containing the three desired GBR color ranges: blue,yellow, red
+    colorBoundaries =[
+                    ([45, 0, 0], [145, 95, 55]),
+                    ([25, 160, 180], [95, 230, 255]),
+                    ([50, 50, 175], [125, 115, 250]),
+                    ]
+    #loop through the ranges
+    for (lowerBound, upperBound) in colorBoundaries:
+        #openCV needs numpy arrays, cannot omit this
+        lower = np.array(lowerBound, dtype = "uint8")
+        upper = np.array(upperBound, dtype = "uint8")
+        #turns pixels in range white, else black
+        colorMask = cv2.inRange(sourceImage, lower, upper)
+
+        #finds union of image and mask
+        maskedSourceImage = cv2.bitwise_and(sourceImage, sourceImage, mask = colorMask)
+        #blurs photo slightly to reduce false positives
+        blurredMaskImage = cv2.bilateralFilter(maskedSourceImage, 11, 17, 17)
+        edgyPhoto = cv2.Canny(blurredMaskImage, 100, 200)
+
+        #now we will find the actual edges in the photo by looking for contours
+        (_, contours, _) = cv2.findContours(edgyPhoto.copy(), cv2.RETR_TREE,
+                            cv2.CHAIN_APPROX_SIMPLE)
+        #contours = sorted(contours, key = cv2.contourArea, reverse = True)
+        screenContour = None #initialize variable
+
+        for cntr in contours:
+            #creates an approximation of the contours
+            peri = cv2.arcLength(cntr, True)
+            approx = cv2.approxPolyDP(cntr, 0.01 * peri, True)
+            #checks how many points in each contour, four points => rectangle
+            if len(approx) >= 4 and len(approx) <= 6:
+                (x, y, w, h) = cv2.boundingRect(approx)
+                aspectRatio = w / float(h)
+
+                #compute the solidity of the original contours
+                area = cv2.contourArea(cntr)
+                hullArea = cv2.contourArea(cv2.convexHull(cntr))
+                solidity = area / float(hullArea)
+
+                #check to see if the width, height, and solidity and aspect ratio
+                #fall within the appropriate bounds
+                keepDims = w > 25 and h > 25
+                keepSolidity = solidity > 0.9
+                keepAspectRatio = aspectRatio >= 0.8 and aspectRatio <= 1.2
+
+                #check to see if all the tests were passed
+                if keepDims and keepSolidity and keepAspectRatio:
+                    #draw an outline around the target and update the status
+                    cv2.drawContours(blurredMaskImage, [approx], -1, (255, 255, 0), 4)
+                    status = "Target aquired"
+                    print(status)
+                else:
+                    print("Not Aquired", keepDims, keepSolidity, keepAspectRatio)
+
+        #show the filtered images
+        cv2.imshow("images", np.hstack([sourceImage, blurredMaskImage]))
+        #cv2.imshow("Aquired Target", maskedSourceImage)
+        if cv2.waitKey(25) and 0xFF == ord('q'):
+            break
+        
+cap.release()
+cv2.destroyAllWindows()
