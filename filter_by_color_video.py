d955df6f20bc2b86a535c0fcbbbf63d76a2aa9c6
Tyler Burt <tyler.burtt@gmail.com>
2017-11-06 14:40:01 -0500

    determine orientation of targets

--- filter_by_color_video.py
+++ filter_by_color_video.py
@@ -1,6 +1,36 @@
 import numpy as np
 import cv2
 
+def checkIfContourMatches(cntr):
+    #creates an approximation of the contours
+    peri = cv2.arcLength(cntr, True)
+    approx = cv2.approxPolyDP(cntr, 0.01 * peri, True)
+    #checks how many points in each contour, four points => rectangle
+    if len(approx) >= 4 and len(approx) <= 6:
+        (x, y, w, h) = cv2.boundingRect(approx)
+        aspectRatio = w / float(h)
+
+        #compute the solidity of the original contours
+        area = cv2.contourArea(cntr)
+        hullArea = cv2.contourArea(cv2.convexHull(cntr))
+        solidity = area / float(hullArea)
+
+        #check to see if the width, height, and solidity and aspect ratio
+        #fall within the appropriate bounds
+        keepDims = w > 25 and h > 25
+        keepSolidity = solidity > 0.9
+        keepAspectRatio = aspectRatio >= 0.8 and aspectRatio <= 1.2
+
+        #check to see if all the tests were passed
+        if keepDims and keepSolidity and keepAspectRatio:
+            #draw an outline around the target and update the status
+            cv2.drawContours(blurredMaskImage, [approx], -1, (255, 255, 0), 4)
+            #return center of target to determine orientation of targets
+            return ((x+x+w)/2,(y+y+h)/2)
+        else:
+            return False
+
+
 cap = cv2.VideoCapture(0)
 
 while(True):
@@ -23,6 +53,8 @@ while(True):
                     ([25, 160, 180], [95, 230, 255]),
                     ([50, 50, 175], [125, 115, 250]),
                     ]
+    #make empty list to put centers of targets in
+    targets = []
     #loop through the ranges
     for (lowerBound, upperBound) in colorBoundaries:
         #openCV needs numpy arrays, cannot omit this
@@ -41,42 +73,35 @@ while(True):
         (_, contours, _) = cv2.findContours(edgyPhoto.copy(), cv2.RETR_TREE,
                             cv2.CHAIN_APPROX_SIMPLE)
         #contours = sorted(contours, key = cv2.contourArea, reverse = True)
-        screenContour = None #initialize variable
-
+        screenContour = None #initialize variable (center of target)
+        
         for cntr in contours:
-            #creates an approximation of the contours
-            peri = cv2.arcLength(cntr, True)
-            approx = cv2.approxPolyDP(cntr, 0.01 * peri, True)
-            #checks how many points in each contour, four points => rectangle
-            if len(approx) >= 4 and len(approx) <= 6:
-                (x, y, w, h) = cv2.boundingRect(approx)
-                aspectRatio = w / float(h)
-
-                #compute the solidity of the original contours
-                area = cv2.contourArea(cntr)
-                hullArea = cv2.contourArea(cv2.convexHull(cntr))
-                solidity = area / float(hullArea)
-
-                #check to see if the width, height, and solidity and aspect ratio
-                #fall within the appropriate bounds
-                keepDims = w > 25 and h > 25
-                keepSolidity = solidity > 0.9
-                keepAspectRatio = aspectRatio >= 0.8 and aspectRatio <= 1.2
-
-                #check to see if all the tests were passed
-                if keepDims and keepSolidity and keepAspectRatio:
-                    #draw an outline around the target and update the status
-                    cv2.drawContours(blurredMaskImage, [approx], -1, (255, 255, 0), 4)
-                    status = "Target aquired"
-                    print(status)
-                else:
-                    print("Not Aquired", keepDims, keepSolidity, keepAspectRatio)
-
+            screenContour = checkIfContourMatches(cntr)
+            if(screenContour):
+                break
+        if not screenContour:
+            print("target not found")
+        else:
+            print("target found")
+            targets.append(screenContour)
+            
         #show the filtered images
         cv2.imshow("images", np.hstack([sourceImage, blurredMaskImage]))
         #cv2.imshow("Aquired Target", maskedSourceImage)
-        if cv2.waitKey(25) and 0xFF == ord('q'):
-            break
+    #determine orientation of targets if all are found
+    if len(targets) == 3:
+        print("all targets found")
+        a, b, c = targets
+        #formula derived from slopes between points to determine orientation
+        val = (b[1] - a[1])*(b[0] - a[0]) - (c[1] - b[1])*(c[0] - b[0])
+        if val > 0:
+            print("clockwise")
+        else:
+            print("counterclockwise")
+    else:
+        print("some targets not found")
+    if cv2.waitKey(25) and 0xFF == ord('q'):
+        break
         
 cap.release()
 cv2.destroyAllWindows()
